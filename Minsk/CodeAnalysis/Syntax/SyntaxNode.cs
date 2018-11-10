3239f8cd723c3c5643aaf426e36adab4be08f679
David Rettenbacher <spoiltnr1@gmx.at>
2018-11-10 19:31:45 +0100

    Add SyntaxNode.Span

--- Minsk/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Minsk/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -1,10 +1,21 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Minsk.CodeAnalysis.Syntax
 {
     public abstract class SyntaxNode
     {
         public abstract SyntaxKind Kind { get; }
+
+        public virtual TextSpan Span
+        {
+            get
+            {
+                var first = GetChildren().First().Span;
+                var last = GetChildren().Last().Span;
+                return TextSpan.FromBounds(first.Start, last.End);
+            }
+        }
 
         public abstract IEnumerable<SyntaxNode> GetChildren();
     }
