e7bc0288cc5ebb6b5ccae5443d13701e081d796c
David Rettenbacher <spoiltnr1@gmx.at>
2018-11-19 00:19:26 +0100

    Reorganize evaluation tests Ensure parser doesn't loop infinitely on malformed block Ensure binder doesn't crash when binding fabricated identifiers

--- Minsk.Tests/CodeAnalysis/EvaluationTests.cs
+++ Minsk.Tests/CodeAnalysis/EvaluationTests.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Minsk.CodeAnalysis;
 using Minsk.CodeAnalysis.Syntax;
 using Xunit;
-using System;
 
 namespace Minsk.Tests.CodeAnalysis
 {
@@ -21,11 +21,6 @@ namespace Minsk.Tests.CodeAnalysis
         [InlineData("3 == 3", true)]
         [InlineData("12 != 3", true)]
         [InlineData("3 != 3", false)]
-        [InlineData("true == true", true)]
-        [InlineData("false == false", true)]
-        [InlineData("true == false", false)]
-        [InlineData("false != false", false)]
-        [InlineData("true != false", true)]
         [InlineData("3 < 4", true)]
         [InlineData("5 < 4", false)]
         [InlineData("4 <= 4", true)]
@@ -36,22 +31,24 @@ namespace Minsk.Tests.CodeAnalysis
         [InlineData("4 >= 4", true)]
         [InlineData("5 >= 4", true)]
         [InlineData("4 >= 5", false)]
+        [InlineData("false == false", true)]
+        [InlineData("true == false", false)]
+        [InlineData("false != false", false)]
+        [InlineData("true != false", true)]
         [InlineData("true", true)]
         [InlineData("false", false)]
         [InlineData("!true", false)]
         [InlineData("!false", true)]
-
         [InlineData("{ var a = 0 (a = 10) * a }", 100)]
-
         [InlineData("{ var a = 0 if a == 0 a = 10 a }", 10)]
         [InlineData("{ var a = 0 if a == 4 a = 10 a }", 0)]
         [InlineData("{ var a = 0 if a == 0 a = 10 else a = 5 a }", 10)]
         [InlineData("{ var a = 0 if a == 4 a = 10 else a = 5 a }", 5)]
         [InlineData("{ var i = 10 var result = 0 while i > 0 { result = result + i i = i - 1} result }", 55)]
         [InlineData("{ var result = 0 for i = 1 to 10 { result = result + i } result }", 55)]
-        public void Evaluator_Evaluate_returns_correct_values(string text, object expectedResult)
+        public void Evaluator_Computes_CorrectValues(string text, object expectedValue)
         {
-            AssertValue(text, expectedResult);
+            AssertValue(text, expectedValue);
         }
 
         [Fact]
@@ -67,55 +64,27 @@ namespace Minsk.Tests.CodeAnalysis
                     var [x] = 5
                 }
             ";
+
             var diagnostics = @"
                 Variable 'x' is already declared.
             ";
+
             AssertDiagnostics(text, diagnostics);
         }
+
         [Fact]
-        public void Evaluator_Name_Reports_Undefined()
-        {
-            var text = @"[x] * 10";
-            var diagnostics = @"
-                Variable 'x' doesn't exist.
-            ";
-            AssertDiagnostics(text, diagnostics);
-        }
-        [Fact]
-        public void Evaluator_Assigned_Reports_Undefined()
-        {
-            var text = @"[x] = 10";
-            var diagnostics = @"
-                Variable 'x' doesn't exist.
-            ";
-            AssertDiagnostics(text, diagnostics);
-        }
-        [Fact]
-        public void Evaluator_Assigned_Reports_CannotAssign()
+        public void Evaluator_BlockStatement_NoInfiniteLoop()
         {
             var text = @"
                 {
-                    let x = 10
-                    x [=] 0
-                }
+                [)][]
             ";
+
             var diagnostics = @"
-                Variable 'x' is read-only and cannot be assigned to.
+                Unexpected token <CloseParenthesisToken>, expected <IdentifierToken>.
+                Unexpected token <EndOfFileToken>, expected <CloseBraceToken>.
             ";
-            AssertDiagnostics(text, diagnostics);
-        }
-        [Fact]
-        public void Evaluator_Assigned_Reports_CannotConvert()
-        {
-            var text = @"
-                {
-                    var x = 10
-                    x = [true]
-                }
-            ";
-            var diagnostics = @"
-                Cannot convert type 'System.Boolean' to 'System.Int32'.
-            ";
+
             AssertDiagnostics(text, diagnostics);
         }
 
@@ -129,9 +98,11 @@ namespace Minsk.Tests.CodeAnalysis
                         x = 10
                 }
             ";
+
             var diagnostics = @"
                 Cannot convert type 'System.Int32' to 'System.Boolean'.
             ";
+
             AssertDiagnostics(text, diagnostics);
         }
 
@@ -145,9 +116,11 @@ namespace Minsk.Tests.CodeAnalysis
                         x = 10
                 }
             ";
+
             var diagnostics = @"
                 Cannot convert type 'System.Int32' to 'System.Boolean'.
             ";
+
             AssertDiagnostics(text, diagnostics);
         }
 
@@ -161,11 +134,14 @@ namespace Minsk.Tests.CodeAnalysis
                         result = result + i
                 }
             ";
+
             var diagnostics = @"
                 Cannot convert type 'System.Boolean' to 'System.Int32'.
             ";
+
             AssertDiagnostics(text, diagnostics);
         }
+
         [Fact]
         public void Evaluator_ForStatement_Reports_CannotConvert_UpperBound()
         {
@@ -176,42 +152,117 @@ namespace Minsk.Tests.CodeAnalysis
                         result = result + i
                 }
             ";
+
             var diagnostics = @"
                 Cannot convert type 'System.Boolean' to 'System.Int32'.
             ";
+
             AssertDiagnostics(text, diagnostics);
         }
 
         [Fact]
-        public void Evaluator_Unary_Reports_Undefined()
+        public void Evaluator_NameExpression_Reports_Undefined()
+        {
+            var text = @"[x] * 10";
+
+            var diagnostics = @"
+                Variable 'x' doesn't exist.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_NameExpression_Reports_NoErrorForInsertedToken()
+        {
+            var text = @"[]";
+
+            var diagnostics = @"
+                Unexpected token <EndOfFileToken>, expected <IdentifierToken>.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_UnaryExpression_Reports_Undefined()
         {
             var text = @"[+]true";
+
             var diagnostics = @"
                 Unary operator '+' is not defined for type 'System.Boolean'.
             ";
-            AssertDiagnostics(text, diagnostics);
-        }
-        [Fact]
-        public void Evaluator_Binary_Reports_Undefined()
-        {
-            var text = @"10 [*] false";
-            var diagnostics = @"
-                Binary operator '*' is not defined for types 'System.Int32' and 'System.Boolean'.
-            ";
+
             AssertDiagnostics(text, diagnostics);
         }
 
-        private static void AssertValue(string text, object expectedResult)
+        [Fact]
+        public void Evaluator_BinaryExpression_Reports_Undefined()
+        {
+            var text = @"10 [*] false";
+
+            var diagnostics = @"
+                Binary operator '*' is not defined for types 'System.Int32' and 'System.Boolean'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_AssignmentExpression_Reports_Undefined()
+        {
+            var text = @"[x] = 10";
+
+            var diagnostics = @"
+                Variable 'x' doesn't exist.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_AssignmentExpression_Reports_CannotAssign()
+        {
+            var text = @"
+                {
+                    let x = 10
+                    x [=] 0
+                }
+            ";
+
+            var diagnostics = @"
+                Variable 'x' is read-only and cannot be assigned to.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        [Fact]
+        public void Evaluator_AssignmentExpression_Reports_CannotConvert()
+        {
+            var text = @"
+                {
+                    var x = 10
+                    x = [true]
+                }
+            ";
+
+            var diagnostics = @"
+                Cannot convert type 'System.Boolean' to 'System.Int32'.
+            ";
+
+            AssertDiagnostics(text, diagnostics);
+        }
+
+        private static void AssertValue(string text, object expectedValue)
         {
             var syntaxTree = SyntaxTree.Parse(text);
             var compilation = new Compilation(syntaxTree);
             var variables = new Dictionary<VariableSymbol, object>();
-
             var result = compilation.Evaluate(variables);
 
             Assert.Empty(result.Diagnostics);
-
-            Assert.Equal(expectedResult, result.Value);
+            Assert.Equal(expectedValue, result.Value);
         }
 
         private void AssertDiagnostics(string text, string diagnosticText)
@@ -220,15 +271,20 @@ namespace Minsk.Tests.CodeAnalysis
             var syntaxTree = SyntaxTree.Parse(annotatedText.Text);
             var compilation = new Compilation(syntaxTree);
             var result = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
+
             var expectedDiagnostics = AnnotatedText.UnindentLines(diagnosticText);
+
             if (annotatedText.Spans.Length != expectedDiagnostics.Length)
                 throw new Exception("ERROR: Must mark as many spans as there are expected diagnostics");
+
             Assert.Equal(expectedDiagnostics.Length, result.Diagnostics.Length);
+
             for (var i = 0; i < expectedDiagnostics.Length; i++)
             {
                 var expectedMessage = expectedDiagnostics[i];
                 var actualMessage = result.Diagnostics[i].Message;
                 Assert.Equal(expectedMessage, actualMessage);
+
                 var expectedSpan = annotatedText.Spans[i];
                 var actualSpan = result.Diagnostics[i].Span;
                 Assert.Equal(expectedSpan, actualSpan);
