7a54f8020ff589ed7c9a71369f00329230e9ce47
David Rettenbacher <spoiltnr1@gmx.at>
2019-05-09 20:03:00 +0200

    Move GetLastToken() to SyntaxNode

--- Minsk/CodeAnalysis/Syntax/SyntaxNode.cs
+++ Minsk/CodeAnalysis/Syntax/SyntaxNode.cs
@@ -22,6 +22,15 @@ namespace Minsk.CodeAnalysis.Syntax
 
         public abstract IEnumerable<SyntaxNode> GetChildren();
 
+        public SyntaxToken GetLastToken()
+        {
+            if (this is SyntaxToken token)
+                return token;
+
+            // A syntax node should always contain at least 1 token.
+            return GetChildren().Last().GetLastToken();
+        }
+
         public void WriteTo(TextWriter writer)
         {
             PrettyPrint(writer, this);
